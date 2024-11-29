6f0442463aa8368b2075bf024d0c83cf20691753
Kranthi <kunkudupala@gmail.com>
2024-11-29 12:22:39 +0530

    testcase for newlines as delimiters and implementation

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -19,5 +19,9 @@ describe "calculator" do
     it 'returns the sum of multiple numbers' do
       expect(c.add("1,2,3,4")).to eq(10)
     end
+
+    it 'handles newlines as delimiters' do
+      expect(c.add("1\n2,3")).to eq(6)
+    end
   end
 end
