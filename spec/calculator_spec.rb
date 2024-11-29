1fc4744b86c26b8cea88acc03720263de84134ff
Kranthi <kunkudupala@gmail.com>
2024-11-29 11:59:58 +0530

    testcase for multiple numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -15,5 +15,9 @@ describe "calculator" do
     it 'returns the sum of two numbers' do
       expect(c.add("1,2")).to eq(3)
     end
+
+    it 'returns the sum of multiple numbers' do
+      expect(c.add("1,2,3,4")).to eq(10)
+    end
   end
 end
